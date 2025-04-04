df70e019623e9c1940d1603532beaddbba07552c
Abdullah M. Al-Awlaqi <ama.awlaqi@gmail.com>
2025-04-04 09:22:10 +0300

    test activities xaml file

---
+++ Tests/UiPathTeam.SharePoint.Activities.Tests/ActivityTemplateWorkflowTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Activities.XamlIntegration;
+using System.Activities;
+using Xunit;
+
+namespace UiPathTeam.SharePoint.Activities.Tests
+{
+    public class ActivityTemplateWorkflowTests
+    {
+        [Fact]
+        public void Test()
+        {
+            ActivityXamlServicesSettings settings = new ActivityXamlServicesSettings
+            {
+                CompileExpressions = true
+            };
+
+
+            Activity workflow = ActivityXamlServices.Load("TestAllActivities.xaml", settings);
+            WorkflowInvoker.Invoke(workflow);
+
+            Console.WriteLine("Press <enter> to exit");
+            Console.ReadLine();
+        }
+
+    }
+}
